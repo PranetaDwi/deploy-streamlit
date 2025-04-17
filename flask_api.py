2f700659f5a5cc5637a13383dcb3c1d28de94beb
verlinof <ajikoko.zx@gmail.com>
2025-04-17 17:03:23 +0700

    Fix: Test

--- flask_api.py
+++ flask_api.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 # Koneksi ke MongoDB
 client = MongoClient("mongodb+srv://neta_sic:neta_sic@backenddb.rfmwzg6.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
 db = client["locations"]
-collection = db["realtime_location"]
+realtime_collection = db["realtime_location"]
+vulnerable_location_collection = db["vulnerable_location"]
+
+# FS Grid
 fs = gridfs.GridFS(db)
 
 @app.route('/upload', methods=['POST'])
@@ -19,17 +22,36 @@ def upload_audio():
     file = request.files['file']
     device_id = request.form.get('device_id') 
 
-    lon = request.form.get('lon') 
-
     if not device_id:
         return {'message': 'Device ID tidak boleh kosong'}, 400
 
     if file.filename == '':
         return {'message': 'Nama file kosong'}, 400
-    
 
     file_id = fs.put(file, device_id=device_id ,filename=file.filename, content_type=file.content_type, uploadDate=datetime.datetime.utcnow())
     return {'message': 'Upload berhasil', 'device_id':str(device_id), 'file_id': str(file_id)}, 200
+
+@app.route('/vulnerable-location', methods=['POST'])
+def post_vulnerable_location():
+    device_id = request.form.get('device_id')
+    lon = request.form.get('lon')
+    lat = request.form.get('lat')
+
+    if not lat:
+        return {'message': 'Lat tidak boleh kosong'}, 400
+    if not lon:
+        return {'message': 'Lon tidak boleh kosong'}, 400
+    if not device_id:
+        return {'message': 'Device ID tidak boleh kosong'}, 400
+
+    result = vulnerable_location_collection.insert_one({
+        'device_id': device_id,
+        'lat': float(lat),
+        'lon': float(lon),
+        'timestamp': datetime.datetime.utcnow()
+    })
+
+    return {'message': 'Berhasil mengirim data', 'device_id':str(device_id), 'lon': str(lon), lat: str(lat)}, 200
 
 @app.route('/post-location', methods=['POST'])
 def post_lang_lot():
@@ -48,7 +70,7 @@ def post_lang_lot():
     if not device_id:
         return {'message': 'Device ID tidak boleh kosong'}, 400
     
-    result = collection.insert_one({
+    result = realtime_collection.insert_one({
     'device_id': device_id,
     'lat': float(lat),
     'lon': float(lon),
